725f80418cd62d6187962a81b2811da87bbc1c76
Mohamed Sherif <kf0127@klivvr.com>
2023-09-12 12:25:03 +0300

    New Era

--- nvim/lua/msherif/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- vim.g["gruvbox_material_background"] = "hard"
--- vim.g["gruvbox_material_better_performance"] = 1
-require("rose-pine").setup({
-  variant = 'dawn'
-})
-
-local status, _ = pcall(vim.cmd, "colorscheme rose-pine-dawn")
-
--- nvim/lua/msherif/plugins/harpoon.lua
+++
@@ -1,7 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-local keymap = vim.keymap
-
-keymap.set("n", "<leader>a", mark.add_file)
-keymap.set("n", "<leader>he", ui.toggle_quick_menu)
