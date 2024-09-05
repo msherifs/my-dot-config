e27f34b7bf36e2493e2e18376e8532661c0df836
Mohammed Sherif <mohamedsherif@Mohamed-Sherif.local>
2024-09-05 12:34:50 +0300

    Updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -36,6 +36,6 @@ return {
 	priority = 1000,
 	opts = {},
 	config = function()
-		vim.cmd("colorscheme tokyonight-night")
+		vim.cmd("colorscheme tokyonight-storm")
 	end,
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,15 +9,21 @@ config.initial_rows = 30
 config.initial_cols = 120
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Tokyo Night Storm (Gogh)"
 config.window_background_opacity = 1
 
 config.font = wezterm.font({
 	family = "Maple Mono NF",
 	harfbuzz_features = { "ss01", "ss03", "zero" },
 })
+
+-- config.font = wezterm.font({
+-- 	family = "JetBrainsMono Nerd Font",
+-- 	harfbuzz_features = { "ss02" },
+-- })
+
 config.font_size = 12
-config.line_height = 1.25
+config.line_height = 1.4
 
 -- and finally, return the configuration to wezterm
 return config
