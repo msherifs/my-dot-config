c5b4ec26a93dc473ba387634e8c9162f960bb3ef
Mohammed Sherif <mohamedsherif@Mohamed-Sherif.local>
2025-02-20 19:29:59 +0200

    tokyo-maple

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
-	config = function(_, _opts)
-		vim.cmd("colorscheme catppuccin-frappe")
+	config = function()
+		vim.cmd("colorscheme tokyonight-night")
 	end,
 }
