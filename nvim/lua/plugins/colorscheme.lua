e3dc486bd00bd2cfc8df210048a1a9ffab8592fd
Mohammed Sherif <mohamedsherif@Mohamed-Sherif.local>
2025-04-18 17:22:38 +0200

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,15 @@
 return {
-	"folke/tokyonight.nvim",
+	"olivercederborg/poimandres.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		style = "night",
-		transparent = true,
-		terminal_colors = true,
-		styles = {
-			comments = { italic = true },
-			keywords = { italic = true },
-			sidebars = "transparent", -- Make sidebars like NvimTree transparent
-			floats = "transparent", -- Make floating windows like LSP hints transparent
-		},
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
+	config = function()
+		require("poimandres").setup({
+			disable_background = true,
+		})
+	end,
 
-		vim.cmd.colorscheme("tokyonight")
+	-- optionally set the colorscheme within lazy config
+	init = function()
+		vim.cmd("colorscheme poimandres")
 	end,
 }
