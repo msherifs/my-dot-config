d0864af7d5ec8a2df3368cc60043a948107451eb
Mohammed Sherif <mohamedsherif@Mohamed-Sherif.local>
2025-05-01 20:45:26 +0300

    refresh 1-may

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,23 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
+		require("rose-pine").setup({
+			variant = "moon", -- auto, main, moon, or dawn
+			dark_variant = "moon", -- main, moon, or dawn
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+			styles = {
+				bold = true,
+				italic = true,
+				transparency = true,
+			},
 		})
 	end,
 
 	-- optionally set the colorscheme within lazy config
 	init = function()
-		vim.cmd("colorscheme catppuccin-mocha")
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
